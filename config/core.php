048c13c5f8c3f3d845165cbf16b53ef2cb1e9c68
adikgh <adiikgh@gmail.com>
2023-05-07 17:12:34 +0600

    CSS

--- config/core.php
+++ config/core.php
@@ -68,7 +68,7 @@
 
    // setting
    $site = mysqli_fetch_array(db::query("select * from `site` where id = 1"));
-   $ver = 1.15;
+   $ver = 1.16;
    $site_set = [
       'header' => true,
       'menu' => true,
